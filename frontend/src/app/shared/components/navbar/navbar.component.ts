54aba9a56cfce95b114c5b1e3ad5f930b590f021
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-03-21 11:43:52 +0100

    SETUP : added a test component navbar

---
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isLoggedIn = false;
+
+  login() {
+    this.isLoggedIn = true;
+    console.log('User logged in');
+  }
+}
