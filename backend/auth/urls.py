15aa5672edb40a305705e45bcb92844524a37b3d
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-04-11 13:55:24 +0200

    FEATURE/Backend : created auth API route to send a rest password link

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from auth.views.register import RegisterView
+from auth.views.forgotten_password import ForgottenPasswordView
 from auth.views.verify_email import VerifyEmailView
 from auth.views.login import CookieTokenObtainPairView
 from auth.views.user_details import UserDetailsView
@@ -11,6 +12,7 @@ urlpatterns = [
     path("login", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh", CookieTokenRefreshView.as_view(), name="token_refresh"),
     path("register", RegisterView.as_view(), name="register"),
+    path("forgotten-password", ForgottenPasswordView.as_view(), name="register"),
     path(
         "verify-email/<uidb64>/<token>",
         VerifyEmailView.as_view(),
