0c30e1a9fddb510d3ab4f5f4fb5c87574055ea38
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-04-22 22:33:28 +0200

    SETUP added a button that can toggle languages in navbar

--- frontend/src/app/core/utils/translate-loader-factory.ts
+++ frontend/src/app/core/utils/translate-loader-factory.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
-import { environment } from "../../../environments/environment";
 
 export function translateLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, `http://localhost:5000/i18n/`, ".json");
+  return new TranslateHttpLoader(http, `/i18n/`, ".json");
 }
--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterLink } from "@angular/router";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatMenuModule } from "@angular/material/menu";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { AuthService } from "../../../core/services/auth.service";
 import { ToastService } from "../../../core/services/toast.service";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
@@ -14,23 +13,30 @@ import { TranslateModule, TranslateService } from "@ngx-translate/core";
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
-  imports: [
-    CommonModule,
-    TranslateModule,
-    MatButtonToggleModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    RouterLink,
-  ],
+  imports: [CommonModule, TranslateModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  readonly locales = [
+    { code: "en", flag: "🇬🇧" },
+    { code: "fr", flag: "🇫🇷" },
+  ];
+
+  flag = "🇬🇧";
+
   constructor(
-    private translateService: TranslateService,
+    public translateService: TranslateService,
     private authService: AuthService,
     private toastService: ToastService
   ) {}
+
+  ngOnInit(): void {
+    const detectedLang = this.translateService.getBrowserLang();
+    if (this.locales.some(locale => locale.code === detectedLang)) {
+      this.translateService.use(detectedLang as string);
+    } else {
+      this.translateService.use("en");
+    }
+  }
 
   get isAuthenticated() {
     return this.authService.isAuthenticated;
@@ -46,9 +52,11 @@ export class NavbarComponent {
     if (!success) this.toastService.showError("Error logging out, try again");
   }
 
-  protected readonly locales = ["en", "fr"];
+  protected changeLanguage(): void {
+    const currentIndex = this.locales.findIndex(locale => locale.code === this.translateService.currentLang);
+    const nextIndex = (currentIndex + 1) % this.locales.length;
+    const nextLocale = this.locales[nextIndex];
 
-  protected changeLanguage(locale: string): void {
-    this.translateService.use(locale);
+    this.translateService.use(nextLocale.code);
   }
 }
