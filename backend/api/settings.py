5cc05b3ca781900163df77737c847bc7324d255a
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-04-23 17:32:10 +0200

    SETUP/Backend : created a trades django app with models and migrations

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "accounts",
     "cards",
     "card_collections",
+    "trades",
 ]
 
 MIDDLEWARE = [
---
+++ backend/trades/urls.py
@@ -0,0 +1,5 @@
+# from card_collections.views.my_collection import MyCollectionView
+
+# urlpatterns = [
+#     path("user/collection", MyCollectionView.as_view(), name="my_collection"),
+# ]
