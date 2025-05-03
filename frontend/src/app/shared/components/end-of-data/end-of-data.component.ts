9551d4ec72ca0f7903003250f536901ac03249a8
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-05-03 14:26:48 +0200

    UX : added ng-content to end-of-data component to adapt it to different contexts

--- frontend/src/app/shared/components/end-of-data/end-of-data.component.ts
+++ frontend/src/app/shared/components/end-of-data/end-of-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from "@angular/core";
+import { Component, ContentChildren, ElementRef, input, QueryList } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TranslateModule } from "@ngx-translate/core";
 import { MatIconModule } from "@angular/material/icon";
@@ -10,7 +10,4 @@ import { MatIconModule } from "@angular/material/icon";
   styleUrls: ["./end-of-data.component.scss"],
   imports: [CommonModule, TranslateModule, MatIconModule],
 })
-export class EndOfDataComponent {
-  message = input<string>();
-  isVisible = input<boolean>();
-}
+export class EndOfDataComponent {}
