39531201416aef4bb05db664bc20eb556b63c65e
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-05-06 00:48:57 +0200

    FEATURE : added data fetching and display in profile-card component, as well as a button to copy/paste trained IDs

--- frontend/src/app/features/public-profiles/profile-card/profile-card.component.ts
+++ frontend/src/app/features/public-profiles/profile-card/profile-card.component.ts
@@ -1,23 +1,60 @@
 import { Component, input, OnInit, signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { environment } from "../../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 interface UserPublicProfile {
-  "username": string
-  "tcgpId": string,
-  "bio": string,
-  "avatarUrl": string
+  username: string;
+  tcgpId: string;
+  bio: string;
+  avatarUrl: string;
 }
 
 @Component({
   selector: "app-profile-card",
   templateUrl: "./profile-card.component.html",
   styleUrls: ["./profile-card.component.scss"],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
 })
 export class ProfileCardComponent implements OnInit {
   username = input<string | null>(null);
 
+  private apiUrl = environment.apiUrl;
+  fileServerUrl = environment.fileServerUrl;
+
+  copyTrainerIdIcon = signal<string>("content_copy");
+
+  userInfo = signal<UserPublicProfile>({
+    username: "loading ...",
+    tcgpId: "loading ...",
+    bio: "loading ...",
+    avatarUrl: "loading ...",
+  });
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
     console.log("PublicProfilePageComponent initialized, username :", this.username);
+
+    this.http.get<UserPublicProfile>(`${this.apiUrl}/users/${this.username()}/info`).subscribe({
+      next: data => {
+        this.userInfo.set(data);
+      },
+      error: error => {
+        console.error("Error fetching user data:", error);
+      },
+    });
+  }
+
+  copyTrainerId() {
+    if (!this.userInfo().tcgpId) return;
+    navigator.clipboard.writeText(this.userInfo().tcgpId);
+    this.copyTrainerIdIcon.set("check");
+    setTimeout(() => {
+      this.copyTrainerIdIcon.set("content_copy");
+    }, 1500);
   }
 }
