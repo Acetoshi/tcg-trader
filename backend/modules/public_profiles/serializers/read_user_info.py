64f175ba63f1d0a24f6ce2c38fcce9806b837149
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2025-05-05 17:29:09 +0200

    FEATURE : Backend : created public endpoint to get public user info

---
+++ backend/modules/public_profiles/serializers/read_user_info.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class ReadUserInfo(serializers.Serializer):
+    username = serializers.CharField()
+
+    def validate(self, attrs):
+        try:
+            user = User.objects.get(username=attrs["username"])
+            attrs["user"] = user
+        except User.DoesNotExist:
+            raise serializers.ValidationError("This user doesn't exist")
+        return attrs
+
+    def read_user_info(self, **kwargs):
+        user = self.validated_data["user"]
+
+        user_info = {
+            "username": user.username,
+            "tcgpId": user.tcgp_id,
+            "bio": user.bio,
+            "avatarUrl": user.avatar_url,
+        }
+
+        return user_info
